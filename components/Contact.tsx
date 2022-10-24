35637b19a493ab7f72bb0e969e7888ef5dc9597c
Ankush263 <ankushbanik263@gmail.com>
2022-10-24 09:25:56 +0530

    update

---
+++ components/Contact.tsx
@@ -0,0 +1,158 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
+// import ContactImg from '../public/assets/contact.jpg';
+import contact from '../public/contact.jpg'
+
+const Contact = () => {
+
+  const handleClick = () => {
+    if(typeof window !== 'undefined') {
+      window.location.replace("mailto:ankushbanik263@gmail.com")
+    }
+  }
+  
+  return (
+    <div id='contact' className='w-full lg:h-screen'>
+      <div className='max-w-[1240px] m-auto px-2 py-16 w-full '>
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+          Contact
+        </p>
+        <h2 className='py-4'>Get In Touch</h2>
+        <div className='grid lg:grid-cols-5 gap-8'>
+          {/* left */}
+          <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
+            <div className='lg:p-4 h-full '>
+              <div>
+                <Image
+                  className='rounded-xl hover:scale-105 ease-in duration-300'
+                  src={contact}
+                  alt='/'
+                />
+              </div>
+              <div>
+                <h2 className='py-2'>Ankush Banik</h2>
+                <p>Web3.0 Developer</p>
+                <p className='py-4'>
+                  I am available for full-time positions. Contact
+                  me and let&apos;s talk.
+                </p>
+              </div>
+              <div>
+                <p className='uppercase pt-8'>Connect With Me</p>
+                <div className='flex items-center justify-between py-4'>
+                  <a
+                    href='https://www.linkedin.com/in/ankush-banik-b61bb6214/'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                      <FaLinkedinIn />
+                    </div>
+                  </a>
+                  <a
+                    href='https://github.com/Ankush263'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                      <FaGithub />
+                    </div>
+                  </a>
+
+                  <div onClick={handleClick} className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <AiOutlineMail />
+                  </div>
+                  <Link href='/resume'>
+                    <a>
+                      <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <BsFillPersonLinesFill />
+                      </div>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* right */}
+          <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
+            <div className='p-4'>
+              <form
+                action='https://getform.io/f/4b27795c-8ae5-4d64-b261-38b7b41e21ad'
+                method='POST'
+                encType='multipart/form-data'
+              >
+                <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+                  <div className='flex flex-col'>
+                    <label className='uppercase text-sm py-2'>Name</label>
+                    <input
+                      className='border-2 rounded-lg p-3 flex border-gray-300 bg-black'
+                      type='text'
+                      name='name'
+                    />
+                  </div>
+                  <div className='flex flex-col'>
+                    <label className='uppercase text-sm py-2'>
+                      Phone Number
+                    </label>
+                    <input
+                      className='border-2 rounded-lg p-3 flex border-gray-300 bg-black'
+                      type='text'
+                      name='phone'
+                    />
+                  </div>
+                </div>
+                <div className='flex flex-col py-2'>
+                  <label className='uppercase text-sm py-2'>Email</label>
+                  <input
+                    className='border-2 rounded-lg p-3 flex border-gray-300 bg-black'
+                    type='email'
+                    name='email'
+                  />
+                </div>
+                <div className='flex flex-col py-2'>
+                  <label className='uppercase text-sm py-2'>Subject</label>
+                  <input
+                    className='border-2 rounded-lg p-3 flex border-gray-300 bg-black'
+                    type='text'
+                    name='subject'
+                  />
+                </div>
+                <div className='flex flex-col py-2'>
+                  <label className='uppercase text-sm py-2'>Message</label>
+                  <textarea
+                    className='border-2 rounded-lg p-3 border-gray-300 bg-black'
+                    rows={10}
+                    name='message'
+                  ></textarea>
+                </div>
+                <button className='w-full p-4 text-gray-100 mt-4 shadow-none'>
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className='flex justify-center py-12'>
+          <Link href='/'>
+            <a>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <HiOutlineChevronDoubleUp
+                  className='text-[#5651e5]'
+                  size={30}
+                />
+              </div>
+            </a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
