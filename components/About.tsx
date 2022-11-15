4714f0c747e77158b37b487b1bdb6be5e8972206
Ankush263 <ankushbanik263@gmail.com>
2022-11-15 14:11:14 +0530

    update

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import AboutImg from '../public/one.jpg';
+import AboutImg from '../public/two.jpg';
 
 const About = () => {
   return (
